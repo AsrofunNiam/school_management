56f12428e25f3fbc47133d938ffac88c8e05e10a
Yahya Adi <yahya.adi@vneu.co.id>
2024-02-14 09:42:41 +0700

    update db schema

--- table_migrate_queries.sql
+++ table_migrate_queries.sql
@@ -1,11 +1,11 @@
 CREATE TABLE "teachers" (
-  "id" varchar PRIMARY KEY,
-  "name" varchar NOT NULL
+  "id" bigserial PRIMARY KEY,
+  "user_id" varchar NOT NULL
 );
 
 CREATE TABLE "students" (
-  "id" varchar PRIMARY KEY,
-  "name" varchar NOT NULL
+  "id" bigserial PRIMARY KEY,
+  "user_id" varchar NOT NULL
 );
 
 CREATE TABLE "class_groups" (
@@ -19,10 +19,11 @@ CREATE TABLE "classes" (
   "name" varchar NOT NULL
 );
 
-CREATE TABLE "student_class" (
+CREATE TABLE "student_classes" (
   "id" bigserial PRIMARY KEY,
-  "student_id" varchar NOT NULL,
-  "class_id" varchar NOT NULL
+  "student_id" bigint NOT NULL,
+  "class_id" varchar NOT NULL,
+  "period" varchar NOT NULL
 );
 
 CREATE TABLE "subjects" (
@@ -30,30 +31,53 @@ CREATE TABLE "subjects" (
   "name" varchar NOT NULL
 );
 
+CREATE TABLE "teacher_subjects" (
+  "id" bigserial PRIMARY KEY,
+  "teacher_id" bigint NOT NULL,
+  "subject_id" bigint NOT NULL,
+  "period" varchar NOT NULL
+);
+
 CREATE TABLE "subject_reports" (
   "id" bigserial PRIMARY KEY,
-  "subject_id" bigint NOT NULL,
-  "teacher_id" bigint NOT NULL,
+  "created_at" timestamptz DEFAULT (now()),
+  "teacher_subject_id" bigint NOT NULL,
   "student_class_id" bigint NOT NULL,
   "mid_exam_result" int,
   "final_exam_result" int,
   "seasonal_exam_result" int[]
 );
 
-COMMENT ON COLUMN "teachers"."id" IS 'from nip';
-
-COMMENT ON COLUMN "students"."id" IS 'from nis';
+CREATE TABLE "users" (
+  "id" varchar PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "role" varchar NOT NULL
+);
 
 COMMENT ON COLUMN "class_groups"."id" IS 'kode jurusan';
 
+COMMENT ON COLUMN "student_classes"."period" IS 'tahun awal TA';
+
+COMMENT ON COLUMN "teacher_subjects"."period" IS 'tahun awal TA';
+
+COMMENT ON COLUMN "users"."id" IS 'from nip or nis';
+
+COMMENT ON COLUMN "users"."role" IS 'enum teacher, student';
+
+ALTER TABLE "teachers" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "students" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
 ALTER TABLE "classes" ADD FOREIGN KEY ("class_groups_id") REFERENCES "class_groups" ("id");
 
-ALTER TABLE "student_class" ADD FOREIGN KEY ("student_id") REFERENCES "students" ("id");
+ALTER TABLE "student_classes" ADD FOREIGN KEY ("student_id") REFERENCES "students" ("id");
 
-ALTER TABLE "student_class" ADD FOREIGN KEY ("class_id") REFERENCES "classes" ("id");
+ALTER TABLE "student_classes" ADD FOREIGN KEY ("class_id") REFERENCES "classes" ("id");
 
-ALTER TABLE "subject_reports" ADD FOREIGN KEY ("subject_id") REFERENCES "subjects" ("id");
+ALTER TABLE "teacher_subjects" ADD FOREIGN KEY ("teacher_id") REFERENCES "teachers" ("id");
 
-ALTER TABLE "subject_reports" ADD FOREIGN KEY ("teacher_id") REFERENCES "teachers" ("id");
+ALTER TABLE "teacher_subjects" ADD FOREIGN KEY ("subject_id") REFERENCES "subjects" ("id");
 
-ALTER TABLE "subject_reports" ADD FOREIGN KEY ("student_class_id") REFERENCES "student_class" ("id");
+ALTER TABLE "subject_reports" ADD FOREIGN KEY ("teacher_subject_id") REFERENCES "teacher_subjects" ("id");
+
+ALTER TABLE "subject_reports" ADD FOREIGN KEY ("student_class_id") REFERENCES "student_classes" ("id");
